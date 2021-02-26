9b09b376b28e7491acdf6446f6908b0d0bafabb7
suehtamCruz <matheuscz3110@gmail.com>
2021-02-26 12:44:08 -0300

    Updade : criado arquivo com a string de conexão

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const {conectString} = require('../lock');
 
-mongoose.connect('',
-{useNewUrlParser: true});
+const conection = mongoose.connect(conectString,  {useNewUrlParser: true , useUnifiedTopology: true }  );
+if(conection){
+    console.log("conectado ao servidor");
+}else{
+    console.log("não conectado")
 
+}
 
 app.listen(8000);
