ad6f55de44c9485bf0bc11613983f09b65786990
suehtamCruz <matheuscz3110@gmail.com>
2021-03-21 11:32:12 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.listen(8000);
+// app.listen(8000);
 
 app.use('/user',userRoute);
 app.use('/cards',cardRoute);
